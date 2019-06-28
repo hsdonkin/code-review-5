2d8c964d060365271868a9d8944f5131663f0962
Hugh Donkin <hsdonkin@gmail.com>
2019-06-28 11:19:50 -0700

    lay out specs and tests for planet age calculator

---
+++ readme.md
@@ -0,0 +1,11 @@
+
+Specs:
+
+| Behavior | Input | Output |
+| ------------- |:-------------:| -----:|
+| Take an age and return it divided by 0.24 | 100 years | 416.666 years |
+| Take an age and return it divided by 0.62 | 100 years | 161.290 |
+| Take an age and return it divided by 1.88 | 100 years | 53.19 |
+| Take an age and return it divided by 11.86 | 100 years | 8.431 |
+| Given a base life expectancy, take an age, return the difference between their age and the base life expectancy | 30 years, 88 base | 58 years |
+| Take the difference and convert it to years on Mercury, Venus, etc | 58 years | 241.66, 93.54, etc |
---
+++ spec/birthday-spec.js
@@ -0,0 +1,27 @@
+import {} from "../src/birthday.js";
+
+
+describe ('planet age', function(){
+
+  it('should take an age and return it divided by 0.24', function(){
+    expect().nothing();
+  })
+
+  it('should take an age and return it divided by 0.62', function(){
+    expect().nothing();
+  })
+  it('should take an age and return it divided by 1.88', function(){
+    expect().nothing();
+  })
+  it('should take an age and return it divided by 11.86', function(){
+    expect().nothing();
+  })
+  it('take a base life expenctancy and a sample age, and return the difference between the two', function(){
+    expect().nothing();
+  })
+  it('take the difference between age and expectancy, and return it in Mercury years, Venus years, etc', function(){
+    expect().nothing();
+  })
+
+
+})
