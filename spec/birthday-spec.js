77673a9cca294e8c59037e519a6d49a37ad394aa
Hugh Donkin <hsdonkin@gmail.com>
2019-06-28 11:50:44 -0700

    implement mercury and venus calcs

--- spec/birthday-spec.js
+++ spec/birthday-spec.js
@@ -1,19 +1,19 @@
-import {} from "../src/birthday.js";
+import {inputAgeDate, currentDate, mercuryAgeCalc} from "./../src/birthday.js";
 
 
 describe ('planet age', function(){
 
-  it('should take an age and return it divided by 0.24', function(){
-    expect().nothing();
+  it('should take an age and return it in Mercury years, divided by 0.24', function(){
+    expect(mercuryAgeCalc(inputAgeDate, currentDate)).toEqual(416);
   })
 
-  it('should take an age and return it divided by 0.62', function(){
+  it('should take an age and return it in Venus years, divided by 0.62', function(){
+    expect(venusAgeCalc(inputAgeDate, currentDate)).toEqual(161);
+  })
+  it('should take an age and return it in Mars years, divided by 1.88', function(){
     expect().nothing();
   })
-  it('should take an age and return it divided by 1.88', function(){
-    expect().nothing();
-  })
-  it('should take an age and return it divided by 11.86', function(){
+  it('should take an age and return it in Jupiter years, divided by 11.86', function(){
     expect().nothing();
   })
   it('take a base life expenctancy and a sample age, and return the difference between the two', function(){
--- src/birthday.js
+++ src/birthday.js
@@ -1,1 +1,11 @@
 
+export let inputAgeDate = new Date ("June 27, 1919");
+export let currentDate = new Date();
+
+export function mercuryAgeCalc(age, currentDate) {
+    return Math.floor((currentDate.getFullYear() - age.getFullYear())/0.24);
+}
+
+export function venusAgeCalc(age, currentDate) {  
+    return Math.floor((currentDate.getFullYear() - age.getFullYear())/0.62);
+}
